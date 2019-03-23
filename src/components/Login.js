36ce25edd352f248eb490a9f6c5858dacf3d6259
Son Trinh <son.trinh@employmenthero.com>
2019-03-23 19:15:56 +0700

    restyling login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,10 +3,12 @@ import setDisplayName from 'recompose/setDisplayName';
 import compose from 'recompose/compose';
 import withProps from 'recompose/withProps';
 import gql from 'graphql-tag';
-import get from 'lodash/fp/get';
+import styled from 'styled-components';
 import { reduxForm, Field } from 'redux-form';
 import { connect } from 'react-redux';
 import { graphql } from 'react-apollo';
+
+import Input from './Input';
 
 const LOGIN_MUTATION = gql`
   mutation LoginMutation($email: String!, $password: String!) {
@@ -18,50 +20,84 @@ const LOGIN_MUTATION = gql`
 
 const FORM_NAME = 'LOGIN_FORM';
 
-const InputField = ({ type, name, id, input }) => (
-  <input
-    className="pa2 input-reset ba bg-transparent w-100 measure"
-    type={type}
-    name={name}
-    id={id}
-    onChange={get('onChange')(input)}
-  />
-);
+const Section = styled.div`
+  width: 20%;
+`;
+
+const styles = {
+  inputStyle: {
+    borderRadius: '20px',
+    textIndent: '35px',
+    height: '40px',
+  },
+  iconStyle: {
+    position: 'absolute',
+    top: '10px',
+    left: '15px',
+    fontSize: '20px',
+  },
+  submitButtonStyle: {
+    width: '20%',
+    height: '40px',
+    borderRadius: '20px',
+    fontSize: '16px',
+    margiTop: '4px',
+  },
+};
 
 const PureLoginForm = ({ handleSubmit }) => (
-  <form onSubmit={handleSubmit}>
-    <legend className="ph0 mh0 fw6 clip">Sign Up</legend>
-    <div className="mt3">
-      <label className="db fw4 lh-copy f6" htmlFor="email-address">
-        Email address
-      </label>
-      <Field id="email" name="email" component={InputField} type="email" />
+  <div className="flex justify-center">
+    <div style={{ flexGrow: 1 }}>
+      <form onSubmit={handleSubmit}>
+        <div className="flex mt3 justify-center">
+          <Section>
+            <label className="db f4 lh-copy" htmlFor="userName">
+              User name
+            </label>
+            <div style={{ position: 'relative' }}>
+              <i style={styles.iconStyle} className="fas fa-user" />
+              <Field
+                id="userName"
+                name="userName"
+                component={Input}
+                style={styles.inputStyle}
+              />
+            </div>
+          </Section>
+        </div>
+        <div className="flex mt3 justify-center">
+          <Section>
+            <label className="db f4 lh-copy" htmlFor="password">
+              Password
+            </label>
+            <div style={{ position: 'relative' }}>
+              <i style={styles.iconStyle} class="fas fa-lock" />
+              <Field
+                id="password"
+                name="password"
+                component={Input}
+                type="password"
+                style={styles.inputStyle}
+              />
+            </div>
+          </Section>
+        </div>
+        <div className="flex mt3 justify-center">
+          <button
+            className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6"
+            style={styles.submitButtonStyle}
+            onClick={handleSubmit}
+          >
+            Login
+          </button>
+        </div>
+      </form>
     </div>
-    <div className="mt3">
-      <label className="db fw4 lh-copy f6" htmlFor="password">
-        Password
-      </label>
-      <Field
-        id="password"
-        name="password"
-        component={InputField}
-        type="password"
-      />
-    </div>
-    <div className="mt3">
-      <button
-        className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6"
-        onClick={handleSubmit}
-      >
-        Login
-      </button>
-    </div>
-  </form>
+  </div>
 );
 
 const prepareProps = withProps(({ mutate }) => ({
   onSubmit: values => {
-    console.log('values', values);
     mutate({
       variables: values,
     });
